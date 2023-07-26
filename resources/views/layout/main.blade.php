45a4ab32f5bf88e469cd9829ad71c9c523a80a09
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2023-07-26 10:44:25 +0700

    logo dan show foto

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>BiFast</title>
-    <link rel="shortcut icon" href="/images/logoBiFast.png" />
+    <title>Djagad Land Group</title>
+    <link rel="shortcut icon" href="images/logologo.png" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
      {{-- Bootstrp Icons --}}
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
