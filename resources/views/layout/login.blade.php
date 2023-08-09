d4fc3d2dd9032671745d06122e8e5e9ba5c9fd07
firdausbiaf <=>
2023-08-09 11:21:10 +0700

    login dan register

--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -3,14 +3,42 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"> --}}
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="path/to/your-custom-js-file.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
-    <style>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+{{-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script> --}}
 
-.login-block {
+    <style>
+        @import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+        .login-block{
+            background: #DE6262;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to bottom, #FFB88C, #DE6262);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to bottom, #FFB88C, #DE6262); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            float:left;
+            width:100%;
+            padding : 50px 0;
+        }
+        .banner-sec{background:url(https://www.djagadland.com/wp-content/uploads/2021/09/Type-57-1-600x450.jpg)  no-repeat left bottom; background-size:cover; min-height:500px; border-radius: 0 10px 10px 0; padding:0;}
+        .container{background:#fff; border-radius: 10px; box-shadow:15px 20px 0px rgba(0,0,0,0.1);}
+        .carousel-inner{border-radius:0 10px 10px 0;}
+        .carousel-caption{text-align:left; left:5%;}
+        .login-sec{padding: 50px 30px; position:relative;}
+        .login-sec .copy-text{position:absolute; width:80%; bottom:20px; font-size:13px; text-align:center;}
+        .login-sec .copy-text i{color:#FEB58A;}
+        .login-sec .copy-text a{color:#3c3d8f;}
+        .login-sec h2{margin-bottom:30px; font-weight:800; font-size:30px; color: #3c3d8f;}
+        .login-sec h2:after{content:" "; width:100px; height:5px; background:#3c3d8f; display:block; margin-top:20px; border-radius:3px; margin-left:auto;margin-right:auto}
+        .btn-login{background: #4b4d9a; color:#fff; font-weight:600;}
+        .banner-text{width:70%; position:absolute; bottom:40px; padding-left:20px;}
+        .banner-text h2{color:#fff; font-weight:600;}
+        .banner-text h2:after{content:" "; width:100px; height:5px; background:#FFF; display:block; margin-top:20px; border-radius:3px;}
+        .banner-text p{color:#fff;}
+
+        .login-block {
             background: linear-gradient(to bottom, #d6dffc, #100f70);
             
             float: left;
@@ -20,19 +48,63 @@
             justify-content: center;
             align-items: center;
         }
+        .carousel-item.active img {
+            width: 100%;
+            height: auto; /* Set the desired height for the carousel (in this example, 100vh will make it full-screen) */
+            object-fit: cover; /* This ensures the image maintains its aspect ratio */
+        }
 
-    .carousel-item.active img {
-        width: 100%;
-        height: auto; /* Set the desired height for the carousel (in this example, 100vh will make it full-screen) */
-        object-fit: cover; /* This ensures the image maintains its aspect ratio */
-    }
-
-    @media (max-width: 768px) {
+        @media (max-width: 768px) {
             .login-block {
-                padding: 30px 0;
+                        padding: 30px 0;
             }
         }
-</style>
+
+        .carousel-caption {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            color: white;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .carousel-caption h1 {
+            font-size: 40px;
+            font-weight: bold;
+            margin-bottom: 10px; /* Set the desired font size for h1 */
+        }
+
+        .carousel-caption h3 {
+            font-size: 25px; /* Set the desired font size for h3 */
+        }
+
+        .carousel-item::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(84, 101, 252, 0.342); /* Blue color with 0.5 opacity */
+        }
+
+        .modal-dialog {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+
+        /* Center the modal content */
+        .modal-content {
+            max-width: 400px;
+            width: 100%;
+            text-align: center;
+        }
+    </style>
 
     {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
