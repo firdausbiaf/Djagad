9c0b833bc1f2e934c7707cc9c6414df211d51832
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2023-08-22 10:47:42 +0700

    tampilan footer user

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -40,86 +40,58 @@
     <!-- Section: Links  -->
     <section class="">
       <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-5 col-lg-6 col-xl-5 mx-auto mb-8">
-          <!-- Content -->
-          <h6 class="text-uppercase fw-bold">D'JAGAD LAND GROUP</h6>
-          <hr class="mb-4 mt-2" style="width: 60px; background-color: #7c4dff; height: 2px" />
-            <div class="row">
-                <div class="col-lg-4 mb-3">
-                    <a class="navbar-brand" href="/">
-                        <img src="{{ asset('images\batu.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
-                    </a>
-                </div>
-                <div class="col-lg-4 mb-3">
-                    <a class="navbar-brand" href="/">
-                        <img src="{{ asset('images\bintan.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
-                    </a>
-                </div>
-                <div class="col-lg-4 mb-3">
-                    <a class="navbar-brand" href="https://www.dparkcity.com/" target="_blank">
-                        <img src="{{ asset('images\dpark.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
-                    </a>
-                </div>
-            </div><br>
-            <div class="row">
-                <div class="col-lg-6 mb-3">
-                    <a class="navbar-brand" href="/">
-                        <img src="{{ asset('images\singosari.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
-                    </a>
-                </div>
-                <div class="col-lg-6 mb-3">
-                    <a class="navbar-brand" href="/">
-                        <img src="{{ asset('images\villa.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
-                    </a>
-                </div>
+      <div class="row mt-3 justify-content-center">
+    <!-- Grid column for logos -->
+    <div class="col-md-6 col-lg-6 col-xl-6 mb-8">
+        <!-- Content -->
+        <h6 class="text-uppercase fw-bold">D'JAGAD LAND GROUP</h6>
+        <hr class="mb-4 mt-2" style="width: 60px; background-color: #7c4dff; height: 2px" />
+        
+        <div class="row justify-content-center mb-3">
+            <div class="col-lg-4 mb-3">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('images\batu.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
+                </a>
             </div>
-
+            <div class="col-lg-4 mb-3">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('images\bintan.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
+                </a>
+            </div>
+            <div class="col-lg-4 mb-3">
+                <a class="navbar-brand" href="https://www.dparkcity.com/" target="_blank">
+                    <img src="{{ asset('images\dpark.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
+                </a>
+            </div>
         </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <!-- <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4"> -->
-          <!-- Links -->
-          <!-- <h6 class="text-uppercase fw-bold">Products</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-          <p><a href="#!" class="text-white">MDBootstrap</a></p>
-          <p><a href="#!" class="text-white">MDWordPress</a></p>
-          <p><a href="#!" class="text-white">BrandFlow</a></p>
-          <p><a href="#!" class="text-white">Bootstrap Angular</a></p>
-        </div> -->
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <!-- <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4"> -->
-          <!-- Links -->
-          <!-- <h6 class="text-uppercase fw-bold">Useful links</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-          <p><a href="#!" class="text-white">Your Account</a></p>
-          <p><a href="#!" class="text-white">Become an Affiliate</a></p>
-          <p><a href="#!" class="text-white">Shipping Rates</a></p>
-          <p><a href="#!" class="text-white">Help</a></p>
-        </div> -->
-        <!-- Grid column -->
-        <style>
-          .address {
-            display: flex;
-            align-items: flex-start;
-          }
-        </style>
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Contact</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-          <p class="address"><i class="fas fa-home mr-3"></i>  Perumahan Persada Bhayangkara Blok F No. 5, Pangetan, Pagentan, Kec. Singosari, Kabupaten Malang, Jawa Timur 65153</p>
-          <p><i class="fas fa-envelope mr-3"></i> marketing@djagadland.com</p>
-          <p><i class="fas fa-phone mr-3"></i> (0341) 3062086</p>
-          <!-- <p><i class="fas fa-print mr-3"></i> (0341) 3062086</p> -->
+        
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('images\singosari.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
+                </a>
+            </div>
+            <div class="col-4">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('images\villa.png') }}" alt="logo" height="35" class="d-inline-block align-text-top">     
+                </a>
+            </div>
         </div>
+    </div>
+    
+    <!-- Grid column for contact -->
+    <div class="col-md-4 col-lg-4 col-xl-4 mb-md-0 mb-4">
+        <!-- Links -->
+        <h6 class="text-uppercase fw-bold">Contact</h6>
+        <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+        <p class="address"><i class="fas fa-home mr-3"></i>  Perumahan Persada Bhayangkara Blok F No. 5, Pangetan, Pagentan, Kec. Singosari, Kabupaten Malang, Jawa Timur 65153</p>
+        <p><i class="fas fa-envelope mr-3"></i> marketing@djagadland.com</p>
+        <p><i class="fas fa-phone mr-3"></i> (0341) 3062086</p>
+        <!-- <p><i class="fas fa-print mr-3"></i> (0341) 3062086</p> -->
+    </div>
+</div>
+
         <!-- Grid column -->
-      </div>
 
       <!-- ... existing HTML code ... -->
 
